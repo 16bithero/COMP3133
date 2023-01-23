1086d4683d57751cbbcac5c6d8c8505f438812fa
16bithero <renzzi.adorador@gmail.com>
2023-01-22 22:48:55 -0500

    first commit, week 2 exercise

---
+++ Lab 1/index.js
@@ -0,0 +1,39 @@
+console.log("Week 2 Lab for Full Stack Dev")
+
+//Import required modules
+const csv = require('csv-parser');
+const fs = require('fs');
+
+//Checks and deletes canada.txt file if exists
+fs.unlink("canada.txt", (err => {
+    if (err) console.log("No file such as 'canada.txt' exists.");
+    else {
+        console.log("File 'canada.txt' successfully deleted.");
+    }
+  }));
+
+//Checks and deletes usa.txt file if exists
+fs.unlink("usa.txt", (err => {
+if (err) console.log("No file such as 'usa.txt' exists.");
+else {
+    console.log("File 'usa.txt' successfully deleted.");
+}
+}));
+
+const csvReader = fs.createReadStream("input_countries.csv")
+
+csvReader.pipe(csv())
+    .on('data', (row) => {
+        const countryData = row.country + "," + row.year + "," + row.population + "\n"
+        if (row.country === 'Canada'){
+            fs.appendFileSync("canada.txt", countryData);
+        }
+        if (row.country === 'United States'){
+            fs.appendFileSync("usa.txt", countryData);
+        }
+        
+
+    })
+    .on('end', () => {
+        console.log('Text files successfully created.');
+    });
